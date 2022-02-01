68f775e38df1846b19c06ee9f883a166cf86f95c
techyminati <sinha.aryan03@gmail.com>
2022-02-01 15:00:10 +0530

    tiare: Setup Kernel Flags
    Change-Id: Id6760bd98251dc55fb337d0fcb10da421fa7e333
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,3 +24,16 @@ TARGET_OTA_ASSERT_DEVICE := tiare,tiarein
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := MSM8937
 TARGET_NO_BOOTLOADER := true
+
+# Kernel
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom androidboot.memcg=true user_debug=30 msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlycon=msm_hsl_uart,0x78B0000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_RAMDISK_OFFSET := 0x01000000
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
+TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8937
+TARGET_KERNEL_VERSION := 3.18
+TARGET_KERNEL_ARCH := arm
+TARGET_KERNEL_CONFIG := tiare_defconfig
