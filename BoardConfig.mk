a0b935b5caee2aa7672cf0fd6bede32bfa0fac0c
techyminati <sinha.aryan03@gmail.com>
2022-02-01 14:55:40 +0530

    tiare: Define device platform
    Change-Id: I8ee40be08df6b8ebd87e629649522a519764db83
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,3 +13,7 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8937
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno308
