415e04ccf085edf2d8674c2a1f69122c49e4501b
techyminati <sinha.aryan03@gmail.com>
2022-02-01 14:55:08 +0530

    tiare: Define device architecture
    Change-Id: If01504c430b23524369fbad70c6163ec440d4067
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,3 +6,10 @@
 
 DEVICE_PATH := device/xiaomi/tiare
 BOARD_VENDOR := xiaomi
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a53
